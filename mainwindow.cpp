305cbe501e2158eec50b4fa70041bdbe90d57152
AlexandraHoinoiu <alexandrahoinoiu@gmail.com>
2020-03-15 01:03:14 +0200

    Install qt, opencv and display an image with gaussian filter

---
+++ mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // read an image
+    cv::Mat image = cv::imread("C:\\Users\\alexandra.hoinoiu\\Desktop\\download.jpg", 1);
+    //Blur the image with 3x3 Gaussian kernel
+    Mat image_blurred_with_3x3_kernel;
+    GaussianBlur(image, image_blurred_with_3x3_kernel, Size(3, 3), 0);
+
+    //Blur the image with 5x5 Gaussian kernel
+    Mat image_blurred_with_5x5_kernel;
+    GaussianBlur(image, image_blurred_with_5x5_kernel, Size(5, 5), 0);
+
+    //Define names of the windows
+    String window_name = "Lotus";
+    String window_name_blurred_with_3x3_kernel = "Lotus Blurred with 3 x 3 Gaussian Kernel";
+    String window_name_blurred_with_5x5_kernel = "Lotus Blurred with 5 x 5 Gaussian Kernel";
+
+    // Create windows with above names
+    namedWindow(window_name);
+    namedWindow(window_name_blurred_with_3x3_kernel);
+    namedWindow(window_name_blurred_with_5x5_kernel);
+
+    // Show our images inside the created windows.
+    imshow(window_name, image);
+    imshow(window_name_blurred_with_3x3_kernel, image_blurred_with_3x3_kernel);
+    imshow(window_name_blurred_with_5x5_kernel, image_blurred_with_5x5_kernel);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
